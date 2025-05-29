8c9ac9ecf5db202af009541d2525cad9f2db96c2
heyboomie <109445551+heyboomie@users.noreply.github.com>
2025-05-29 10:15:39 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import math
+
+vi = 0
+vr = 0
+Cd = 0.47 #only spheres rn sorry
+p = [1.292, 1.269, 1.246, 1.225, 1.204, 1.184] #start at 0c and go up in 5c incraments, in units kg/m^3
+r = 0 #radius in cm
+A = 0
+pi = 3.1415926535
+thetai = 0
+thetar = 0
+g = 9.80665
+hi = 0
+hr = 0
+di = 0
+dr = 0
+riemann = 1
+t = 0
+#cross section area
+r = input("raduis of the object in cm")/100
+A = pi*r*r
+#speeds and stuff
+vi = input("what is the inital veloctity in m/s?")
+thetai = input("what is the inital launch angle?")
+hi = input("what is the initial height at launch in m?")
+#calculations stuff
+
+
